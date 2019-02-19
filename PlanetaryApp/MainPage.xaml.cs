537a3c36892b73d2ea7f6334b5dc94fa49582444
Jean Jacques Uzabumuhire <uzabumuhire@gmail.com>
2019-02-20 00:23:58 +0100

    Displaying list of planets

--- PlanetaryApp/MainPage.xaml.cs
+++ PlanetaryApp/MainPage.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
 using Xamarin.Forms;
 
 namespace PlanetaryApp
@@ -12,6 +8,62 @@ namespace PlanetaryApp
         public MainPage()
         {
             InitializeComponent();
+
+            // Create and populate a List of Planetary names
+            var planets = new ObservableCollection<Planet>()
+            {
+                new Planet
+                {
+                    Name = "Mercury",
+                    Distance = "Distance from Earth: 77 million kilometers"
+                },
+                new Planet
+                {
+                    Name = "Venus",
+                    Distance = "Distance from Earth: 261 million kilometers"
+                },
+                new Planet
+                {
+                    Name = "Earth",
+                    Distance = "Distance from Sun: 149.6 million kilometers"
+                },
+                new Planet
+                {
+                    Name = "Mars",
+                    Distance = "Distance from Earth: 54.6 million kilometers"
+                },
+                new Planet
+                {
+                    Name = "Jupiter",
+                    Distance = "Distance from Earth: 588 million kilometers"
+                },
+                new Planet
+                {
+                    Name = "Saturn",
+                    Distance = "Distance from Earth: 1.2 billion kilometers"
+                },
+                new Planet
+                {
+                    Name = "Uranus",
+                    Distance = "Distance from Earth: 2.6 billion kilometers"
+                },
+                new Planet
+                {
+                    Name = "Neptune",
+                    Distance = "Distance from Earth: 4.3 billion kilometers"
+                }
+
+            };
+
+            // Set the list of planets to our ListView to display the items
+            planetsListView.ItemsSource = planets;
+        }
+
+        public class Planet
+        {
+            public string Name { get; set; }
+
+            public string Distance { get; set; }
         }
     }
 }
